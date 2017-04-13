7fc4ab3538e1ff38d88a909c25ed123f71ade53c
Sebastian Blunt <sebastian@c4k3.net>
2017-04-13 18:14:28 +0200

    Use Makefile instead of compile.sh

--- compile.sh
+++
@@ -1,8 +0,0 @@
-RT_JAR=/usr/lib/jvm/java-8-openjdk/jre/lib/rt.jar
-INCLUDES_DIR=../
-BUKKIT_TARGET=bukkit-1.8.8B1.jar
-DST_DIR=.
-
-javac -Xlint:all -bootclasspath "$RT_JAR:$INCLUDES_DIR/$BUKKIT_TARGET" -d ./ src/*
-jar -cfe $DST_DIR/NoSpam.jar net/simpvp/NoSpam/NoSpam ./*
-rm -rf net/
